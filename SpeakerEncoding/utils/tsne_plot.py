2f62083aa44a151dab44c778e95523e981203a88
Tommy Fox <45125966+tommy-fox@users.noreply.github.com>
2025-04-24 18:49:38 -0700

    Update tsne_plot script

--- SpeakerEncoding/utils/tsne_plot.py
+++ SpeakerEncoding/utils/tsne_plot.py
@@ -1,24 +1,22 @@
 import yaml, argparse
 import torch
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.manifold import TSNE
 from sklearn.preprocessing import LabelEncoder
 
-def load_speaker_embeddings(path):
-    data = torch.load(path)
-    return data['speaker_embeddings'], data['speaker_labels']
 
 def plot_tsne(speaker_embeddings, speaker_labels):
     speaker_embeddings = speaker_embeddings.squeeze(1)
-    speaker_embeddings_numpy = speaker_embeddings.cpu().numpy()
-    speaker_labels_numpy = speaker_labels.cpu().numpy()
+    speaker_embeddings_numpy = speaker_embeddings.numpy()
+    speaker_labels_numpy = np.array(speaker_labels)
     speaker_labels_encoded = LabelEncoder().fit_transform(speaker_labels_numpy)
 
-    tsne = TSNE(n_components=2, perplexity=30, random_state=42)
+    tsne = TSNE(random_state=42)
     speaker_embeddings_2d = tsne.fit_transform(speaker_embeddings_numpy)
 
     plt.figure(figsize=(10, 8))
-    scatter = plt.scatter(speaker_embeddings_2d[:, 0], speaker_embeddings_2d[:, 1], c=speaker_labels_encoded, cmap='tab20', alpha=0.5)
+    scatter = plt.scatter(speaker_embeddings_2d[:, 0], speaker_embeddings_2d[:, 1], c=speaker_labels_encoded, cmap='tab20', alpha=1.0)
     plt.colorbar(scatter, label="Speaker ID")
     plt.title("t-SNE Plot of Speaker Embeddings")
     plt.xlabel("t-SNE Component 1")
@@ -36,5 +34,6 @@ if __name__ == "__main__":
     with open(args.config_file, 'r') as file:
         config = yaml.safe_load(file)
 
-    speaker_embeddings, speaker_labels = load_speaker_embeddings(config['speaker_embedding_path'])
+    embedding_data = torch.load(config['speaker_embedding_path'])
+    speaker_embeddings, speaker_labels = embedding_data['speaker_embeddings'], embedding_data['speaker_labels']
     plot_tsne(speaker_embeddings, speaker_labels)
